71487bb558ab29b782b99d4d75fe980964bbeb1b
Justin McLachlan <justin@boxfirepress.com>
2011-01-19 21:22:55 -0500

    refactored entity index page

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,12 @@ class Entity < ActiveRecord::Base
   belongs_to :story
   belongs_to :user
   
+  def entity_name(entity_type)
+    if entity_type == 1
+      "Person"
+    else
+      "Company"
+    end
+  end
+  
 end
